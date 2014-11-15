3434477f7ec314c8cb033a46132ef12b28feb021
VictorCoronas <Walkerazos@gmail.com>
2014-11-15 13:48:15 +0100

    Update evento-ponente.py

--- _oo/class/evento-ponente.py
+++ _oo/class/evento-ponente.py
@@ -2,5 +2,5 @@ import webapp2
 from google.appengine.ext import ndb
 
 class EventoPonente(ndb.Model):
-	idOrganizacion = ndb.StringProperty()
-	idUsuario = ndb.StringProperty()
+	idEvento = ndb.StringProperty()
+	idPonente = ndb.StringProperty()

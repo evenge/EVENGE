5e9546c1a7e80ec447ee36a60d1f7283430e9caf
VictorCoronas <walkerazos@gmail.com>
2014-11-23 12:47:49 +0100

    añadido el fomrulario usuario

--- index.py
+++ index.py
@@ -18,6 +18,20 @@ formulario = """\
             </form>
             """
 
+formulario2 = """\
+            <form action="/usuario" method="post">
+            <div><label for="nombre">Nombre: </label><input type="text" id="nombre" name="nombre"/></div>
+            <div><label for="apellidos">Apellidos: </label><input type="text" id="apellidos" name="apellidos"/></div>
+            <div><label for="email">Email: </label><input type="text" id="email" name="email"/></div>
+            <div><label for="password">Password: </label><input type="text" id="password" name="password"/></div>
+            <div><label for="telefono">Telefono: </label><input type="text" id="telefono" name="telefono"/></div>
+            <div><label for="twitter">Twitter: </label><input type="text" id="twitter" name="twitter"/></div>
+            <div><label for="web">Web: </label><input type="text" id="web" name="web"/></div>
+            <div><input type="submit" value="Guardar"></div>
+            </form>
+            """
+
+
 class index(webapp2.RequestHandler):
     def get(self):
         template_values = {}
@@ -28,10 +42,15 @@ class insertar_asistente(webapp2.RequestHandler):
     def get(self):
         self.response.write(formulario)
 
+class insertar_usuario(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(formulario2)
+
 
 application = webapp2.WSGIApplication([
     ('/', index),
-    ('/iAsistente', insertar_asistente)
+    ('/iAsistente', insertar_asistente),
+    ('/iUsuario', insertar_usuario)
 ], debug=True)
 
 JINJA_ENVIRONMENT = jinja2.Environment(

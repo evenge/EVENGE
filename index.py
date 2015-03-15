ae7e9d23024a663a01ed5cebaabb13130afc409a
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2015-03-15 11:49:08 +0100

    Dando formato a index.py #232

--- index.py
+++ index.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
-#Evenge - gestor de eventos (events management)
-#Copyright (C) 2014 - desarrollo.evenge@gmail.com
-#Carlos Campos Fuentes | Francisco Javier Exposito Cruz | Ivan Ortega Alba | Victor Coronas Lara
+# Evenge - gestor de eventos (events management)
+# Copyright (C) 2014 - desarrollo.evenge@gmail.com
+# Carlos Campos Fuentes | Francisco Javier Exposito Cruz | Ivan Ortega Alba | Victor Coronas Lara
 #
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 ################################################################
-"""Modulo de inicio.Básicamente maneja las URL, renderiza la plantilla correspondiente
-y pása los parametros a dicha plantilla. Cada clase corresponde a una URL. Ver clase par más información.
+"""Modulo de inicio.Básicamente maneja las URL, renderiza la plantilla
+correspondiente y pása los parametros a dicha plantilla. Cada clase corresponde
+a una URL. Ver clase par más información.
    :author: Carlos Campos Fuentes | Francisco Javier Exposito Cruz | Ivan Ortega Alba | Victor Coronas Lara
    :version: 0.1"""
 
 import os
 import urllib
-from google.appengine.api import users
 from google.appengine.ext import ndb
 from _oo.classes.evento import Evento
 from _oo.model import controladorEvento
@@ -41,9 +41,9 @@ class Index(webapp2.RequestHandler):
         Si está logueado o no
         """
         usuario = controladorUsuario.getUsuarioLogeado(self)
-        if usuario :
+        if usuario:
             eventos = controladorEvento.getEventosAsociados(usuario.key.id())
-            template_values = {'usuario':usuario,'eventos':eventos}
+            template_values = {'usuario': usuario, 'eventos': eventos}
             template = JINJA_ENVIRONMENT.get_template('templates/templateMyEvents.html')
             self.response.write(template.render(template_values))
         else:
@@ -72,7 +72,7 @@ class InsertarEvento(webapp2.RequestHandler):
         nombre = self.request.get('nombre')
         hora = self.request.get('hora')
         fecha = self.request.get('fecha')
-        ca = self.request.get('cantidadAsistentes')
+        cantidadAsistentes = self.request.get('cantidadAsistentes')
         descripcion = self.request.get('descripcion').strip()
         lugar = self.request.get('lugar')
         asistencia = self.request.get('asistencia')
@@ -80,8 +80,11 @@ class InsertarEvento(webapp2.RequestHandler):
         lon = self.request.get('longitud')
         privado = self.request.get('privado')
         idCreador = self.request.get('idUser')
-        ret = controladorEvento.SetEvento(nombre, 1, privado, idCreador, hora, fecha, lugar, lat, lon, descripcion, asistencia);
-        #Aquí se avisaría por email
+        ret = controladorEvento.SetEvento(nombre, 1, privado,
+        idCreador, hora, fecha,
+        cantidadAsistentes, lugar, lat,
+        lon, descripcion, asistencia)
+        # Aquí se avisaría por email
         resp = {'response': True, 'idEvento': ret}
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(resp))
@@ -93,12 +96,13 @@ class InsertarPonente(webapp2.RequestHandler):
     def get(self):
         """Devuelve el formulario para registrar un nuevo ponente"""
         user = controladorUsuario.getUsuarioLogeado(self)
-        if user == False:
+        if user is False:
             self.redirect('/login')
         else:
             template_values = {}
             template = JINJA_ENVIRONMENT.get_template('templates/templatesNewPonente.html')
             self.response.write(template.render(template_values))
+
     def post(self):
         """Inserta un nuevo ponente con los datos recogidos"""
         nombre = self.request.get("nombre").strip()
@@ -108,10 +112,9 @@ class InsertarPonente(webapp2.RequestHandler):
         twitter = self.request.get("twitter").strip()
         web = self.request.get("web").strip()
         idNuevoPonente = controladorPonente.nuevoRegistroPonente(
-            nombre,apellidos,
-            email,telefono,
-            twitter,web
-            )
+            nombre, apellidos,
+            email, telefono,
+            twitter, web)
         self.redirect('/')
 
 # class Evenge(webapp2.RequestHandler):
@@ -135,6 +138,7 @@ class InsertarPonente(webapp2.RequestHandler):
 #         u.put()
 #         return True
 
+
 class MostrarEvento(webapp2.RequestHandler):
     """Es llamada por /miseventos."""
 
@@ -157,21 +161,22 @@ class MostrarEvento(webapp2.RequestHandler):
         idEvento = self.request.get('id')
         evento = controladorEvento.GetEventoById(idEvento)
         asistentes = controladorEvento.getAsistentesEvento(idEvento);
-        if user != False:
+        if user is not False:
             userLogin = True
             numeroEventos = controladorEvento.getEventosAsociadosCount(controladorUsuario.getKey(user))
         if str(controladorUsuario.getKey(user)) == str(evento.idCreador):
             userCreador = True
-        template_values = {'evento':evento,
+        template_values = {'evento': evento,
                            'userLogin': userLogin,
-                           'descripcion':evento.descripcion.replace("\n", "<br />"),
-                           'userCreador':userCreador,
-                           'numeroEventos':numeroEventos,
-                           'id':idEvento,
+                           'descripcion': evento.descripcion.replace("\n", "<br />"),
+                           'userCreador': userCreador,
+                           'numeroEventos': numeroEventos,
+                           'id': idEvento,
                            'usuario': user,
-                           'asistentes':asistentes}
+                           'asistentes': asistentes}
         template = JINJA_ENVIRONMENT.get_template('templates/templateEvents.html')
         self.response.write(template.render(template_values))
+
 
 class MostrarInforme(webapp2.RequestHandler):
     """Es llamada por /misinformes."""
@@ -180,10 +185,10 @@ class MostrarInforme(webapp2.RequestHandler):
         """
         Muestra la plantilla, por ahora sin datos, de la generación de informes.
         """
-
         template_values = {}
         template = JINJA_ENVIRONMENT.get_template('templates/templateReports.html')
         self.response.write(template.render(template_values))
+
 
 class MostrarMiCuenta(webapp2.RequestHandler):
     """Es llamada por /micuenta"""
@@ -197,13 +202,16 @@ class MostrarMiCuenta(webapp2.RequestHandler):
         """
         userLogin = False
         user = controladorUsuario.getUsuarioLogeado(self)
-        if user != False:
+        if user is not False:
             userLogin = True
             numeroEventos = controladorEvento.getEventosAsociadosCount(controladorUsuario.getKey(user))
         usuario = controladorUsuario.getUsuarioLogeado(self)
-        template_values = {'usuario':usuario, "numeroEventos": numeroEventos, "userLogin": userLogin}
+        template_values = {'usuario': usuario,
+                            'numeroEventos': numeroEventos,
+                            'userLogin': userLogin}
         template = JINJA_ENVIRONMENT.get_template('templates/templateUser.html')
         self.response.write(template.render(template_values))
+
 
 class MostrarMisEventos(webapp2.RequestHandler):
     """Es llamada por /miseventos"""
@@ -216,16 +224,18 @@ class MostrarMisEventos(webapp2.RequestHandler):
         """
         usuarioLogeado = controladorUsuario.getUsuarioLogeado(self)
         eventos = controladorEvento.getEventosAsociados(usuarioLogeado.key.id())
-        template_values = {'eventos':eventos}
+        template_values = {'eventos': eventos}
         template = JINJA_ENVIRONMENT.get_template('templates/templateMyEvents.html')
         self.response.write(template.render(template_values))
+
 
 class MostrarMisPonentes(webapp2.RequestHandler):
     def get(self):
         ponentes = controladorPonente.listarPonentes(self)
-        template_values = {'ponentes':ponentes}
+        template_values = {'ponentes': ponentes}
         template = JINJA_ENVIRONMENT.get_template('templates/templatePonentes.html')
         self.response.write(template.render(template_values))
+
 
 class MostrarError(webapp2.RequestHandler):
     """
@@ -239,6 +249,7 @@ class MostrarError(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/templateError.html')
         self.response.write(template.render(template_values))
 
+
 class NuevoUsuario(webapp2.RequestHandler):
     """Es llamada por /registrate"""
     def get(self):
@@ -248,6 +259,7 @@ class NuevoUsuario(webapp2.RequestHandler):
         template_values = {}
         template = JINJA_ENVIRONMENT.get_template('templates/templatesNewUser.html')
         self.response.write(template.render(template_values))
+
     def post(self):
         nombre = self.request.get("nombre").strip()
         apellidos = self.request.get("apellidos").strip()
@@ -257,11 +269,12 @@ class NuevoUsuario(webapp2.RequestHandler):
         web = self.request.get("web").strip()
         password = self.request.get("contrasena").strip()
         idNuevoUsuario = controladorUsuario.nuevoRegistroUsuario(
-            nombre,apellidos,
-            email,telefono,
-            twitter,web,
+            nombre, apellidos,
+            email, telefono,
+            twitter, web,
             password)
         self.redirect('/')
+
 
 class Login(webapp2.RequestHandler):
     """ Es llamada por /login"""
@@ -280,16 +293,17 @@ class Login(webapp2.RequestHandler):
         Si lo es, añade las cookies y queda iniciada la sesión
         """
         contrasena = self.request.get("contrasena").strip()
-        logeado = controladorUsuario.loginCorrecto(self.request.get("email").strip(),contrasena)
+        logeado = controladorUsuario.loginCorrecto(self.request.get("email").strip(), contrasena)
 
-        if logeado != False:
+        if logeado is not False:
             logeado = logeado.get()
-            self.response.headers.add_header('Set-Cookie',"logged=true")
-            self.response.headers.add_header('Set-Cookie',"email="+str(logeado.email))
-            self.response.headers.add_header('Set-Cookie',"key="+str(logeado.key.id()))
+            self.response.headers.add_header('Set-Cookie', "logged=true")
+            self.response.headers.add_header('Set-Cookie', "email=" + str(logeado.email))
+            self.response.headers.add_header('Set-Cookie', "key=" + str(logeado.key.id()))
             self.redirect("/")
         else:
             self.redirect("/login")
+
 
 class Logout(webapp2.RequestHandler):
     """Es llamada por /logout"""
@@ -299,11 +313,12 @@ class Logout(webapp2.RequestHandler):
         Elimina las cookies relacionadas y queda cerrada la sesión
         """
         if self.request.cookies.get("logged") == "true":
-            self.response.headers.add_header('Set-Cookie',"logged=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
-            self.response.headers.add_header('Set-Cookie',"email=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
-            self.response.headers.add_header('Set-Cookie',"key=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
+            self.response.headers.add_header('Set-Cookie', "logged=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
+            self.response.headers.add_header('Set-Cookie', "email=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
+            self.response.headers.add_header('Set-Cookie', "key=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
 
         self.redirect("/")
+
 
 class EliminarEvento(webapp2.RequestHandler):
     """Es llamada por /eliminarEvento"""
@@ -312,18 +327,15 @@ class EliminarEvento(webapp2.RequestHandler):
         """
         Recibe el id del evento a borrar por post, y lo borra de la BD
         """
-        userLogin = False
-        userCreador = False
         user = controladorUsuario.getUsuarioLogeado(self)
         idEvento = self.request.get('id')
-        if user == False:
+        if user is False:
             self.redirect("/")
         else:
             ret = controladorEvento.DeleteEvento(idEvento)
             resp = {'response': ret}
             self.response.headers['Content-Type'] = 'application/json'
             self.response.write(json.dumps(resp))
-
 
 application = webapp2.WSGIApplication([
     ('/', Index),

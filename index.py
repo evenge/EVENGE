e118b01af09d7c2c488d7201e22e2678465b6e18
Fran Exposito <8macau8@gmail.com>
2015-06-14 19:27:37 +0200

    validacion usuario terminada

--- index.py
+++ index.py
@@ -455,7 +455,10 @@ class Login(webapp2.RequestHandler):
         """
         Muestra el formulario de login
         """
-        template_values = {}
+        if (self.request.get("login")):
+            template_values = {'error':True}
+        else:
+            template_values = {}
         template = JINJA_ENVIRONMENT.get_template('templates/templateLogin.html')
         self.response.write(template.render(template_values))
 
@@ -474,7 +477,7 @@ class Login(webapp2.RequestHandler):
             self.response.headers.add_header('Set-Cookie', "key=" + str(logeado.key.id()))
             self.redirect("/")
         else:
-            self.redirect("/login")
+            self.redirect("/login?login=error")
 
 
 class Logout(webapp2.RequestHandler):
@@ -520,7 +523,7 @@ class CrearOrganizacion(webapp2.RequestHandler):
         user = controladorUsuario.getUsuarioLogeado(self)
 
         if user is False:
-            self.response.write(json.dumps({'reponse': 'false'}))
+            self.response.write(json.dumps({'response': 'false'}))
         else:
             nombre = self.request.get('nombre').strip()
             mail = self.request.get('email').strip()
@@ -531,7 +534,7 @@ class CrearOrganizacion(webapp2.RequestHandler):
             controladorOrganizacion.setUsuarioOrganizacion(idOrganizacion, user.key.id())
             controladorUsuario.setOrganizacion(idOrganizacion, user.key.id())
 
-            self.response.write(json.dumps({'reponse': 'true'}))
+            self.response.write(json.dumps({'response': 'true'}))
 
 class InsertarAsistente(webapp2.RequestHandler):
     """Es llamada por /iAsistente"""
@@ -694,14 +697,15 @@ class SubirImagenOrganizacion(webapp2.RequestHandler):
 
 class ModificarUsuario(webapp2.RequestHandler):
     def post(self):
+        u = controladorUsuario.getUsuarioLogeado(self)
         nombre = self.request.get("nombre").strip()
         apellidos = self.request.get("apellidos").strip()
         telefono = self.request.get("telefono").strip()
         twitter = self.request.get("twitter").strip()
         ciudad = self.request.get("ciudad").strip()
         web = self.request.get("web").strip()
-        controladorUsuario.modificarUsuario(nombre, apellidos, telefono, twitter, web, ciudad)
-        self.response.write(json.dumps({'reponse': 'true'}))
+        controladorUsuario.modificarUsuario(nombre, apellidos, telefono, twitter, web, ciudad, u.getKey())
+        self.response.write(json.dumps({'response': 'true'}))
 
 
 
